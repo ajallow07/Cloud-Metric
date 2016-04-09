11f4a474e4886d0cdf6069d0a2b9b166ccfed456
Alieu Jallow <ueilawollaj@gmail.com>
2016-04-09 22:44:27 +0200

    Immplement mapping algorithm

--- mapping.py
+++ mapping.py
@@ -11,67 +11,103 @@ instance that matches or is closest to the choices of input parameters
 2. Methods that takes a flavor instances and compute compute the cost (done)
 3.
 """
-AWS_FLAVORS = [{"t2.nano": {"vCPU": 1,"memory": 0.5}},
-                {"t2.micro": {"vCPU": 1,"memory": 1}},
-                {"t2.small": {"vCPU": 1,"memory": 2}},
-                {"t2.medium" : {"vCPU": 2,"memory": 4}},
-                {"t2.large" : {"vCPU": 2,"memory": 8}},
-                {"m4.large" : {"vCPU": 2,"memory": 8}},
-                {"m3.medium": {"vCPU": 1,"memory": 3.75}},
-                {"m3.xlarge": {"vCPU": 2,"memory": 15}},
-                {"m3.large": {"vCPU": 2,"memory": 7.5}},
-                {"c4.large":{"vCPU":2,"memory": 3.75}},
-                {"c3.large": {"vCPU": 2,"memory": 3.75}},
-                {"r3.large" : {"vCPU": 2,"memory": 15}},
-                {"m4.2xlarge": {"vCPU": 2,"memory": 32}},
-                {"r3.xlarge" : {"vCPU": 4,"memory": 30.5}},
-                {"i2.xlarge": {"vCPU": 4,"memory": 30.5}} ,
-                {"i2.2xlarge" : {"vCPU":8,"memory": 61}},
-                {"i2.4xlarge" : {"vCPU": 16,"memory": 122}},
-                {"i2.8xlarge" : {"vCPU": 32,"memory": 244}},
-                {"c3.xlarge": {"vCPU": 4,"memory": 7.5}},
-                {"c4.xlarge" : {"vCPU": 4,"memory": 7.5}},
-                {"m4.4xlarge" : {"vCPU": 16,"memory": 64}},
-                {"m3.2xlarge": {"vCPU":8,"memory": 30}},
-                {"m4.10xlarge" : {"vCPU": 40,"memory": 160}},
-                {"m4.xlarge" : {"vCPU": 4,"memory": 16}},
-                {"c4.2xlarge": {"vCPU": 8,"memory": 15}},
-                {"c4.4xlarge": {"vCPU": 16,"memory": 30}},
-                {"c4.8xlarge": {"vCPU": 32,"memory": 60}},
-                {"c3.2xlarge" : {"vCPU": 8,"memory": 15}},
-                {"c3.4xlarge",: {"vCPU": 16,"memory": 30}}
-                {"c3.8xlarge": {"vCPU": 32,"memory": 60}},
-                {"g2.2xlarge": {"vCPU": 8,"memory": 15}},
-                {"g2.8xlarge": {"vCPU": 32,"memory": 60}},
-                {"r3.2xlarge": {"vCPU": 8,"memory": 61}},
-                {"r3.4xlarge": {"vCPU": 16,"memory": 122}},
-                {"r3.8xlarge": {"vCPU": 32,"memory": 244}},
-                {"d2.xlarge": {"vCPU": 4,"memory": 30.5}},
-                {"d2.2xlarge" : {"vCPU":8,"memory": 61}},
-                {"d2.4xlarge": {"vCPU":16,"memory": 122}},
-                {"d2.8xlarge": {"vCPU": 22,"memory": 244}}
+AWS_FLAVORS = [{"name": "t2.nano", "param" :{"vCPU": 1,"memory": 0.5}, "mf": 0},
+                {"name":"t2.micro","param": {"vCPU": 1,"memory": 1},"mf": 0},
+                {"name":"t2.small", "param": {"vCPU": 1,"memory": 2},"mf": 0},
+                {"name":"t2.medium", "param" : {"vCPU": 2,"memory": 4},"mf": 0},
+                {"name":"t2.large" , "param": {"vCPU": 2,"memory": 8},"mf": 0},
+                {"name":"m4.large" , "param": {"vCPU": 2,"memory": 8},"mf": 0},
+                {"name":"m3.medium", "param": {"vCPU": 1,"memory": 3.75},"mf": 0},
+                {"name":"m3.xlarge", "param": {"vCPU": 2,"memory": 15},"mf": 0},
+                {"name":"m3.large", "param": {"vCPU": 2,"memory": 7.5},"mf": 0},
+                {"name":"c4.large", "param": {"vCPU":2,"memory": 3.75},"mf": 0},
+                {"name":"c3.large", "param": {"vCPU": 2,"memory": 3.75},"mf": 0},
+                {"name":"r3.large" , "param": {"vCPU": 2,"memory": 15},"mf": 0},
+                {"name":"m4.2xlarge", "param": {"vCPU": 2,"memory": 32},"mf": 0},
+                {"name":"r3.xlarge" , "param": {"vCPU": 4,"memory": 30.5},"mf": 0},
+                {"name":"i2.xlarge", "param": {"vCPU": 4,"memory": 30.5},"mf": 0} ,
+                {"name":"i2.2xlarge", "param" : {"vCPU":8,"memory": 61},"mf": 0},
+                {"name":"i2.4xlarge" , "param": {"vCPU": 16,"memory": 122},"mf": 0},
+                {"name":"i2.8xlarge", "param" : {"vCPU": 32,"memory": 244},"mf": 0},
+                {"name":"c3.xlarge", "param": {"vCPU": 4,"memory": 7.5},"mf": 0},
+                {"name":"c4.xlarge" , "param": {"vCPU": 4,"memory": 7.5},"mf": 0},
+                {"name":"m4.4xlarge", "param" : {"vCPU": 16,"memory": 64},"mf": 0},
+                {"name":"m3.2xlarge", "param": {"vCPU":8,"memory": 30},"mf": 0},
+                {"name":"m4.10xlarge" , "param": {"vCPU": 40,"memory": 160},"mf": 0},
+                {"name":"m4.xlarge" , "param": {"vCPU": 4,"memory": 16},"mf": 0},
+                {"name":"c4.2xlarge", "param": {"vCPU": 8,"memory": 15},"mf": 0},
+                {"name":"c4.4xlarge", "param": {"vCPU": 16,"memory": 30},"mf": 0},
+                {"name":"c4.8xlarge", "param": {"vCPU": 32,"memory": 60},"mf": 0},
+                {"name":"c3.2xlarge" , "param": {"vCPU": 8,"memory": 15},"mf": 0},
+                {"name":"c3.4xlarge", "param": {"vCPU": 16,"memory": 30},"mf": 0},
+                {"name":"c3.8xlarge" , "param": {"vCPU": 32,"memory": 60},"mf": 0},
+                {"name":"g2.2xlarge", "param": {"vCPU": 8,"memory": 15},"mf": 0},
+                {"name":"g2.8xlarge", "param": {"vCPU": 32,"memory": 60},"mf": 0},
+                {"name":"r3.2xlarge", "param": {"vCPU": 8,"memory": 61},"mf": 0},
+                {"name":"r3.4xlarge", "param": {"vCPU": 16,"memory": 122},"mf": 0},
+                {"name":"r3.8xlarge", "param": {"vCPU": 32,"memory": 244},"mf": 0},
+                {"name":"d2.xlarge", "param": {"vCPU": 4,"memory": 30.5},"mf": 0},
+                {"name":"d2.2xlarge", "param": {"vCPU":8,"memory": 61},"mf": 0},
+                {"name":"d2.4xlarge", "param": {"vCPU":16,"memory": 122},"mf": 0},
+                {"name":"d2.8xlarge", "param": {"vCPU": 22,"memory": 244},"mf": 0}
                 ]
 
-GC_FLAVORS = [   {"F1-MICRO" : {"vCPU": 1,"memory": 0.60}},
-                {"G1-SMALL" : {"vCPU": 1,"memory": 1.70}},
-                {"N1-STANDARD-1": {"vCPU": 1,"memory": 3.75}} ,
-                {"N1-STANDARD-2": {"vCPU": 2,"memory": 7.5}},
-                {"N1-STANDARD-4": {"vCPU": 4,"memory": 15}},
-                {"N1-STANDARD-8": {"vCPU": 8,"memory": 30}},
-                {"N1-STANDARD-16": {"vCPU": 16,"memory": 60}},
-                {"N1-STANDARD-32": {"vCPU": 32,"memory": 120}},
-                {"N1-HIGHMEM-2": {"vCPU": 2,"memory": 13}},
-                {"N1-HIGHMEM-4": {"vCPU": 4,"memory": 26}},
-                {"N1-HIGHMEM-8": {"vCPU": 8,"memory": 52}},
-                {"N1-HIGHMEM-16": {"vCPU": 16,"memory": 104}},
-                {"N1-HIGHMEM-32": {"vCPU": 32,"memory":208}},
-                {"N1-HIGHCPU-2": {"vCPU": 2,"memory": 1.80}},
-                {"N1-HIGHCPU-4": {"vCPU": 4,"memory": 3.60},
-                {"N1-HIGHCPU-8": {"vCPU": 8,"memory": 7.20}},
-                {"N1-HIGHCPU-16": {"vCPU": 16,"memory": 14.40}},
-                {"N1-HIGHCPU-32": {"vCPU": 32,"memory": 28.80}}
+GC_FLAVORS = [   {"name":"F1-MICRO" , "param": {"vCPU": 1, "memory": 0.60},"mf": 0},
+                {"name":"G1-SMALL" , "param": {"vCPU": 1, "memory": 1.70},"mf": 0},
+                {"name":"N1-STANDARD-1", "param": {"vCPU": 1, "memory": 3.75},"mf": 0},
+                {"name":"N1-STANDARD-2", "param": {"vCPU": 2, "memory": 7.5},"mf": 0},
+                {"name":"N1-STANDARD-4", "param": {"vCPU": 4, "memory": 15},"mf": 0},
+                {"name":"N1-STANDARD-8", "param": {"vCPU": 8, "memory": 30},"mf": 0},
+                {"name":"N1-STANDARD-16", "param": {"vCPU": 16, "memory": 60},"mf": 0},
+                {"name":"N1-STANDARD-32", "param": {"vCPU": 32, "memory": 120},"mf": 0},
+                {"name":"N1-HIGHMEM-2", "param": {"vCPU": 2, "memory": 13},"mf": 0},
+                {"name":"N1-HIGHMEM-4", "param": {"vCPU": 4, "memory": 26},"mf": 0},
+                {"name":"N1-HIGHMEM-8", "param": {"vCPU": 8, "memory": 52},"mf": 0},
+                {"name":"N1-HIGHMEM-16", "param": {"vCPU": 16, "memory": 104},"mf": 0},
+                {"name":"N1-HIGHMEM-32", "param": {"vCPU": 32, "memory":208},"mf": 0},
+                {"name":"N1-HIGHCPU-2", "param": {"vCPU": 2,"memory": 1.80},"mf": 0},
+                {"name":"N1-HIGHCPU-4", "param": {"vCPU": 4, "memory": 3.60},"mf": 0},
+                {"name":"N1-HIGHCPU-8", "param": {"vCPU": 8, "memory": 7.20},"mf": 0},
+                {"name":"N1-HIGHCPU-16", "param": {"vCPU": 16, "memory": 14.40},"mf": 0},
+                {"name":"N1-HIGHCPU-32", "param": {"vCPU": 32, "memory": 28.80},"mf": 0}
             ]
 
-def getClosestMatchingInstanceInAWS(awsflavors, vcpu, memory):
+def getMatchingInstanceInAWS(awsflavors, vcpu, memory):
+    for flavor in awsflavors:
+        #get the absolute differences in vCPU and memory usages
+        flavor['mf'] = abs(vcpu-flavor['param']['vCPU']) + abs(memory - flavor['param']['memory'])
 
-    return none
+    matchingFavors = []
+    minimumDiff = 10000
+
+    for flavor in awsflavors:
+        if flavor['mf'] < minimumDiff:
+            minimumDiff = flavor['mf']
+
+    for flavor in awsflavors:
+        if flavor["mf"] == minimumDiff:
+            matchingFavors.append(flavor['name'])
+
+
+    return matchingFavors
+
+def getMatchingInstanceInGCE(gceflavors, vcpu, memory):
+    for flavor in gceflavors:
+        #get the absolute differences in vCPU and memory usages
+        flavor['mf'] = abs(vcpu-flavor['param']['vCPU']) + abs(memory - flavor['param']['memory'])
+
+    matchingFavors = []
+    minimumDiff = 10000
+
+    for flavor in gceflavors:
+        if flavor['mf'] < minimumDiff:
+            minimumDiff = flavor['mf']
+
+    for flavor in gceflavors:
+        if flavor["mf"] == minimumDiff:
+            matchingFavors.append(flavor['name'])
+    return matchingFavors
+
+
+if __name__=="__main__":
+    print getMatchingInstanceInGCE(GC_FLAVORS,16,40)
